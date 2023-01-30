6d9b1fc000c78f989554a8c63fe2be4605b2f704
Vibhabakkam <vibhabakkam22@gmail.com>
2023-01-30 11:59:31 +0530

    Day10-assignment

--- day10.js
+++ day10.js
@@ -1,2 +1,9 @@
-var vibha= 254654;
-console.log (vibha);
+var data = 12;
+console.log( " data:" , data);
+console.log("data type:",typeof(data));
+var data2 = "12";
+console.log( " data:" , data2);
+console.log("data type:",typeof(data2));
+var data3 = false
+console.log( " data:" , data3);
+console.log("data type:",typeof(data3));
