a45eb3d35b5f267092f6df10bd98efc01b47976a
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-09 16:32:12 +0530

    day15

---
+++ Day 15.js
@@ -0,0 +1,8 @@
+//logical operators
+a = true;
+b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!(a && b));
+console.log((a && b) && (a || b));
+console.log(((a && b) && (a || b)) || ((a && b)));
