cc6aa0f04e1754ce91da4ba584d9519d6140da78
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-17 23:12:25 +0530

    Create Javascript Questions.js

---
+++ Javascript Questions.js
@@ -0,0 +1,23 @@
+// Q1 - check given Number is odd or even or display the result
+var usarNumber = 89;
+if ((usarNumber % 2) == 0) {
+    console.log("number is even");
+}
+else {
+    console.log("number is odd   ..");
+}
+
+var Statement = 88;
+if ((Statement % 2) == 0) {
+    console.log(" provided number is even");
+}
+else {
+    console.log(" provided number is odd   ..");
+}
+
+// Q2 print the number from 20-40 which are even.
+for ( var i =20; i <= 40; i++){
+    if(i%2===0){
+                     console.log(i)
+    }
+ }
