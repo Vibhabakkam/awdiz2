6495cec6884ac46b171f80a4402762e1f61edfcf
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-19 12:50:46 +0530

    Day22

---
+++ day 22/script.js
@@ -0,0 +1,13 @@
+// function login(vibha) {
+//     // set of code
+//     alert( `welcome vibha`);
+// }
+// login("vibha");
+// console.log( outsidefunction)
+
+function addTodo(){
+    //set of code 
+    var gettingTodos = document.getElementById( 'todosHere') .value;
+    console.log(gettingTodos, "gettingTodos");
+    alert ('Added')
+}
