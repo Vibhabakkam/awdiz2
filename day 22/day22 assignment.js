2070fb888c6250f6f7fd4ee0e4c077ba5e039985
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-20 22:44:53 +0530

    Update day22 assignment.js

---
+++ day 22/day22 assignment.js
@@ -0,0 +1,29 @@
+//pop
+var nums = [10,20,30,40,60,70,80,90,100]
+nums.pop();
+console.log(nums)
+
+//push 
+var city = [ 'mumbai', 'pune','nagpur','delhi', 'kolkata','hyderabad' ]
+city.push('chennai');
+console.log(city);
+
+//shift
+var days = ['monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+days.shift();
+console.log(days);
+
+//unshift
+var nums2 = [1,2,3,4,5,6,7,8,9,10]
+nums2.unshift(11,12);
+console.log(nums2);
+//slice
+console.log(nums2.slice(2));
+
+//splice
+var months = [ 'jan', 'mar','apr','may']
+months.splice(0,1,'feb');
+console.log(months);
+
+
+
