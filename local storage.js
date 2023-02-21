b34289972b8ca4239befc714e5acf1f24d7d76b6
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-21 17:11:27 +0530

    localstorage

---
+++ local storage.js
@@ -0,0 +1,7 @@
+//localStorage
+
+localStorage.setItem('Studentname','Vibha');
+localStorage.setItem( 'age', '22');
+localStorage.setItem( 'university', 'Dbatu');
+localStorage.setItem('percentage','80');
+console.log(localStorage);
